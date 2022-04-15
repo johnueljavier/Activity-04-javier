49488f0958ad22a701c06bff17e97c0a681a9ca5
johnueljavier <awelismjavier11@gmail.com>
2022-04-15 17:37:50 +0800

    added new files

--- user.py
+++ user.py
@@ -2,14 +2,22 @@ import cv2
 import os
 
 while True:
-	pc = input("Your file[insert '.jpg' at the end]:")
+    pc = input("Your file[insert '.jpg' at the end]:")
+    flag = input("\nPress[1] for colored image \n press[0] for grayscale image:")
+    if os.path.exists(pc) and flag == '0':
+        imge= cv2.imread(pc, 0)
+        cv2.imshow(pc,imge)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        break
+    elif os.path.exists(pc) and flag == '1':
+        imge= cv2.imread(pc, 1)
+        cv2.imshow(pc,imge)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        break
 
-	if os.path.exists(pc):
-		imge= cv2.imread(pc, 1)
-		cv2.imshow(pc,imge)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-		break
+    else:
+        print("denied")
 
-	else:
-		print("denied")
+	
